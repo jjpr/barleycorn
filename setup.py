b91887bc229725867bd1b1e922e2c31175c140f0
Jonathan J. Prescott-Roy <jjprescottroy@gmail.com>
2015-05-12 14:27:14 -0400

    Missing cgtypes in setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name='barleycorn',
   version='0.1.0',
-  packages=['barleycorn', 'barleycorn.toolkits', 'cgkit'],
+  packages=['barleycorn', 'barleycorn.toolkits', 'cgkit', 'cgkit.cgtypes'],
   url='https://github.com/jjpr/barleycorn',
   license='',
   author='jjpr',
